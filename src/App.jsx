b2ef180500abc6891e9ef177862e25f6f6f43b9f
saviokh <savio.khoury3@gmail.com>
2026-03-06 12:37:14 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <>
+        <div className="bird-map-page">
+    
+
+      <div className="bird-map-container">
+        <iframe
+          src="https://hixai.com/"
+          title="AI Assistant"
+          className="bird-map-iframe"
+          allowFullScreen
+        />
+      </div>
+    </div>
+    </>
+   
+ 
+  )
+}
+
+export default App
